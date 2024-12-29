08f241b52e172d081be4bc5c0e558d0303f76b65
magn3054 <mdch20@student.aau.dk>
2024-12-30 00:38:53 +0100

    fzdgb

--- hash.php
+++ hash.php
@@ -1,5 +1,25 @@
 <?php
-$password = 'tqr76uuk';
-$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-echo $hashedPassword;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $password = $_POST['password']; // Get the password from user input
+    if (!empty($password)) {
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        echo "Hashed Password: " . $hashedPassword;
+    } else {
+        echo "Please enter a password.";
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Hasher</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="password">Enter Password:</label>
+        <input type="password" id="password" name="password" required>
+        <button type="submit">Hash Password</button>
+    </form>
+</body>
+</html>
